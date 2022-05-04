aecdaa504c7d789ab23131d2b5d3bce994ca3969
Yeshwant Singh <yeshwantsingh6@gmail.com>
2022-05-04 12:53:08 +0530

    Initial commit with folder structure from modified cookiecutter template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Transcription of indian classical music into sargam with time intervals.',
+    author='Yeshwant Singh',
+    license='MIT',
+)
